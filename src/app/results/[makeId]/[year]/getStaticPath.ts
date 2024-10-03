8485cfd52af5932c28191f986afdd3cacb9144db
Skatoro <73762604+Skatoro@users.noreply.github.com>
2024-10-04 00:15:11 +0300

    Whoops

--- src/app/results/[makeId]/[year]/getStaticPath.ts
+++ src/app/results/[makeId]/[year]/getStaticPath.ts
@@ -3,7 +3,6 @@ import {vehicleDates} from "@/constants/vehicleDates";
 import {GetStaticPaths} from "next";
 
 export const getStaticPaths = (async () => {
-    console.log('penis')
     const vehicleResponse = await fetch('https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForVehicleType/car?format=json');
     const vehicleData = await vehicleResponse.json();
     const makeIds: string[] = vehicleData.map((make: IVehicle) => make.MakeId)
